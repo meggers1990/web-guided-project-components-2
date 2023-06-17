1e6e0d1aeeb5cce3dedaef414f5eefbf051c79fc
megan huckabay <huckabay90@gmail.com>
2023-06-17 12:27:53 -0700

    finished guided project

--- src/index.js
+++ src/index.js
@@ -1,39 +1,45 @@
-// 👉 TASK 1- Test out the following endpoints:
+// 👉 TASK 1- Test out the following endpoint:
 
 //  https://dog.ceo/api/breeds/image/random
 
-//  * With Firefox and the Network Tab
+//  * With Postman (HTTP Client with GUI)
+//  * With Chrome and the Network Tab
 //  * With JS using the native fetch [STRETCH]
+
 
 
 // 👉 TASK 2- Select the "entry point", the element
 // inside of which we'll inject our dog cards 
-const entryPoint = null
+const entryPoint = document.querySelector('.entry')
 
 
 // 👉 TASK 3- `dogCardMaker` takes an object and returns a Dog Card.
 // Use this function to build a Card, and append it to the entry point.
 function dogCardMaker({ imageURL, breed }) {
   // instantiating the elements
-  /*
-    <div class="dog-card">
-      <img class="dog-image">
-      <h3>
-    </div>
-  */
-  // set class names, attributes and text
-
-  // create the hierarchy
-
-  // add some interactivity
-
+  const dogCard = document.createElement('div')
+  const image = document.createElement('img')
+  const heading = document.createElement('h3')
+  // setting class names, attributes and text
+  heading.textContent = `Breed: ${breed}`
+  image.src = imageURL
+  image.classList.add('dog-image')
+  dogCard.classList.add('dog-card')
+  // creating the hierarchy
+  dogCard.appendChild(image)
+  dogCard.appendChild(heading)
+  // adding some interactivity
+  dogCard.addEventListener('click', () => {
+    dogCard.classList.toggle('selected')
+  })
   // never forget to return!
+  return dogCard
 }
 
 
 // 👉 TASK 4- Bring the Axios library into the project using one of two methods:
 //    * Traditional way: put another script tag inside index.html (`https://cdn.jsdelivr.net/npm/axios/dist/axios.min.js`)
-//    * Projects with npm: install it with npm and import it into this file
+//    * Projects with npm: install it with npm and import it at the top of this module
 
 
 // 👉 TASK 5- Fetch dogs from `https://dog.ceo/api/breed/{breed}/images/random/{number}`
@@ -41,14 +47,38 @@ function dogCardMaker({ imageURL, breed }) {
 //    * ON FAILURE: log the error to the console
 //    * IN ANY CASE: log "done" to the console
 
+// Make SURE at some point to go over passing a parameter into a function and using that 
+// parameter inside of querySelector. At this point learners haven't seen that
+// and it was formerly a very large pain point in the sprint challenge.
+
 
 // 👉 (OPTIONAL) TASK 6- Wrap the fetching operation inside a function `getDogs`
 // that takes a breed and a count (of dogs)
+function getDogs(breed, count) {
+  axios.get(`https://dog.ceo/api/breed/${breed}/images/random/${count}`)
+    .then(res => {
+      res.data.message.forEach(imageURL => {
+        const dogCard = dogCardMaker({ imageURL, breed })
+        entryPoint.appendChild(dogCard)
+      })
+    })
+    .catch(err => {
+      console.log('Something went wrong', err)
+    })
+    .finally(() => {
+      console.log('done')
+    })
+}
 
 
 // 👉 (OPTIONAL) TASK 7- Put a button in index.html to 'get dogs' and add a click
 // event listener that executes `getDogs`
+document.querySelector('button').addEventListener('click', evt => {
+  getDogs('mastiff', 3)
+  getDogs('appenzeller', 3)
+})
 
 
 // 👉 (OPTIONAL) TASK 8- Import the breeds from `breeds.js`
+// or request them from https://lambda-times-api.herokuapp.com/breeds
 // and loop over them, fetching a dog at each iteration
